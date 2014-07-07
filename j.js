dfbe12cb5cda92520dfd9c4bfc0f95429dc110cc
abhishekkr <abhikumar163@gmail.com>
2014-07-07 14:42:39 +0530

    basic set css query selector, trim
    just the most basic of functionalities for which loading jq doesn't look
    good anymore

---
+++ j.js
@@ -0,0 +1,19 @@
+/* minimal which I mostly require; so gotta go jq */
+
+/* just a css selector */
+function $DOM(elementHint, elementIndex){
+  /* // in case non-css selector gets required 
+  var first_char = elementHint.match(/^./)[0];
+	var rest_chars = elementHint.replace(/^./, ''); */
+    	
+  var str_nodes = document.querySelectorAll(elementHint);
+  if (elementIndex === undefined) {
+    elementIndex = 0;
+  }
+	return str_nodes[elementIndex];
+}
+
+/* trim strings l-r spaces */
+function trim(str){
+  return str.replace(/(^\s+|\s+$)/g, '')
+}
---
+++ tests/tests_j.js
@@ -0,0 +1,26 @@
+function TestDOMWithoutIndex(elementHint, expectedValue){
+	var resultValue;
+	resultValue = trim($DOM(elementHint).innerHTML);
+	QUnit.test("$DOM for " + elementHint, function( assert ) {
+  		assert.equal( resultValue, expectedValue);
+	});
+}
+
+function TestDOMWithIndex(elementHint, elementIndex, expectedValue){
+	var resultValue;
+	resultValue = trim($DOM(elementHint, elementIndex).innerHTML);
+	QUnit.test("$DOM for " + elementHint + " at " + elementIndex, function( assert ) {
+  		assert.equal( resultValue, expectedValue);
+	});
+}
+
+//without index
+TestDOMWithoutIndex("#banner", "Sample001"); //id
+TestDOMWithoutIndex(".testlist", "n/a"); //class
+TestDOMWithoutIndex("b", "Check it out."); //tagname
+TestDOMWithoutIndex("div[name=testr01]", "testr01 testing name"); //property
+//with index
+TestDOMWithIndex("#banner", 0, "Sample001"); //id
+TestDOMWithIndex(".testlist", 1, "nil"); //class
+TestDOMWithIndex("b", 1, "Don't Check."); //tagname
+TestDOMWithIndex("div[name=testr01]", 2, "testr01 testing name once again"); //property
