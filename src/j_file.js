196124ad4742ed07e9f6a3f67f19bdb56d5abffd
AbhishekKr <abhikumar163@gmail.com>
2015-01-19 18:38:37 +0530

    [j_file] loadURI updated to send empty string if 4xx|5xx error

--- src/j_file.js
+++ src/j_file.js
@@ -5,5 +5,9 @@ function loadURI(href){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", href, false);
     xmlhttp.send();
+    var stateX = parseInt(xmlhttp.status / 100);
+    if(stateX == 4 || stateX == 5){
+      return "";
+    }
     return xmlhttp.responseText;
 }
--- tests/tests_j_file.js
+++ tests/tests_j_file.js
@@ -1,10 +1,11 @@
 // testing ../j_file.js
 
 function TestLoadURI(href, expectedValue){
-	resultValue = loadURI(href);
-	QUnit.test("LoadURI for " + href, function( assert ) {
-  		assert.equal( resultValue, expectedValue);
+	var resultValue = loadURI(href);
+	QUnit.test("LoadURI for " + href, function(assert) {
+  		assert.equal(resultValue, expectedValue);
 	});
 }
 
 TestLoadURI("./README.md", "### Tests for the J-LIB");
+TestLoadURI("./NOFILE.md", "");
